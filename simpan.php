c5c66125c6a443e37a2f96112165e20dc220e5bd
Dedy Surya Prihantoro <dedy131098@gmail.com>
2020-09-14 14:37:25 +0700

    Create simpan.php

---
+++ simpan.php
@@ -0,0 +1,30 @@
+<?php
+
+include "koneksi.php";
+
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+if( $conn === false ) {
+    echo "Koneksi Gagal</br>";
+    die( print_r( sqlsrv_errors(), true));
+}
+
+
+$kodebarang = $_POST['kodeBarang'];
+$namabarang = $_POST['namaBarang'];
+$satuan     = $_POST['satuan'];
+
+$tsql = "Insert into barang values('$kodeBarang','$namaBarang','$satuan')";
+$stmt = sqlsrv_query( $conn, $tsql);
+
+if( $stmt === false ) {
+    echo "Error in executing query.</br>";
+    die( print_r( sqlsrv_errors(), true));
+}
+
+header('location:index.php');
+
+sqlsrv_free_stmt( $stmt);
+
+sqlsrv_close( $conn);
+?>
