dce36b559e135b29d70eadcf4ed9145d62e2cbab
Dedy Surya Prihantoro <dedy131098@gmail.com>
2020-09-14 14:37:16 +0700

    Create hapus.php

---
+++ hapus.php
@@ -0,0 +1,28 @@
+<?php
+
+include "koneksi.php";
+
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+if( $conn === false ) {
+    echo "Koneksi Gagal</br>";
+    die( print_r( sqlsrv_errors(), true));
+}
+
+$kodebarang = $_GET['kodebarang'];
+
+$tsql = "Delete barang where kodebarang = '$kodebarang'";
+
+$stmt = sqlsrv_query( $conn, $tsql);
+
+if( $stmt === false ) {
+    echo "Error in executing query.</br>";
+    die( print_r( sqlsrv_errors(), true));
+}
+
+header('location:index.php');
+
+sqlsrv_free_stmt( $stmt);
+
+sqlsrv_close( $conn);
+?>
