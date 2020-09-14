987029b31849970db251d17bfd1888ce810f983c
Dedy Surya Prihantoro <dedy131098@gmail.com>
2020-09-14 12:40:51 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -3,9 +3,9 @@
     $connectionInfo = array( "Database"=>"test", "UID"=>"sa", "PWD"=>"admin1");
     $conn = sqlsrv_connect( $serverName, $connectionInfo);
 
-    if($conn){
+    /*if($conn){
         echo "Koneksi berhasil";
     }else{
         echo "Koneksi gagal";
-    }
+    }*/
 ?>
