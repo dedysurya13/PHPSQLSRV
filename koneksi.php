5344fe20d62f5d5c4630bff7a6251104091c1c8a
Dedy Surya Prihantoro <dedy131098@gmail.com>
2020-09-14 08:28:29 +0700

    Initial commit

---
+++ koneksi.php
@@ -0,0 +1,11 @@
+<?php
+    $serverName = "DEDYSURYAPC\SQLEXPRESS"; //serverName\instanceName
+    $connectionInfo = array( "Database"=>"test", "UID"=>"sa", "PWD"=>"admin1");
+    $conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+    if($conn){
+        echo "Koneksi berhasil";
+    }else{
+        echo "Koneksi gagal";
+    }
+?>
